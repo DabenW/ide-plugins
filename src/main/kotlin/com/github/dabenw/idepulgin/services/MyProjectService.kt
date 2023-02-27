b25eeb6b05ba51454ae238001381580251cd1520
GitHub Action <action@github.com>
2023-02-27 07:37:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/dabenw/idepulgin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dabenw/idepulgin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dabenw.idepulgin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dabenw.idepulgin.MyBundle
 
 class MyProjectService(project: Project) {
 
